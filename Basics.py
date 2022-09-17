b9fcd8278456c4957f12b9dcd8e90445ae8253f7
bsamyakjain <bsamyakjain@gmail.com>
2022-09-17 23:32:46 +0530

    import,args,contolflow

--- Basics.py
+++ Basics.py
@@ -1,2 +1,42 @@
 # slicing always returns a new copy of the anything it performed on
 
+a = list(range(10))
+b=[]
+for i in range(10):
+    b.append(i)
+print(a,b)
+
+for j in range(3):
+    for i in range(10):
+
+        if i==2:
+            continue
+
+        elif i==3:
+            print("Its three")
+
+        elif i==5:
+            break
+
+        elif i==4:
+            print()
+        
+
+        print(i)
+    print("its J ", j)
+
+
+import SamyakLib as sj
+
+print(sj.stars(5))
+
+
+#args and kwargs 
+
+def myFun(arg1, *argv):
+	print("First argument :", arg1)
+	for arg in argv:
+		print("Next argument through *argv :", arg)
+
+
+myFun('Hello')
